f370a17c90aaeb240048013c49c1eff83ca8ca36
RAJU BARDE <125546485+rajuforbear@users.noreply.github.com>
2024-06-17 17:01:06 +0530

    changes

--- src/screens/ads.js
+++ src/screens/ads.js
@@ -7,8 +7,8 @@ export const Addsid = {
       Interstitial: TestIds.INTERSTITIAL,
     },
     android: {
-      BANNER: TestIds.BANNER, // 'ca-app-pub-3339897183017333/5721016782',
-      Interstitial: TestIds.INTERSTITIAL, //'ca-app-pub-3339897183017333/4244283582',
+      BANNER: 'ca-app-pub-3339897183017333/5721016782',
+      Interstitial: 'ca-app-pub-3339897183017333/4244283582',
       Interstitial2: 'ca-app-pub-3339897183017333/7197749989',
     },
   }),
